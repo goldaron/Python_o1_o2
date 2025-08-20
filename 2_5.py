f3768350a118e8c5db2a99b35f509d1650a0b33e
Goldaron <70323504+goldaron@users.noreply.github.com>
2025-08-20 16:54:48 +0300

    vitos tehtävä

--- 2_5.py
+++ 2_5.py
@@ -4,24 +4,18 @@
 #Yksi leiviskä on 20 naulaa.
 #Yksi naula on 32 luotia.
 #Yksi luoti on 13,3 grammaa.
-import math
 
 leiviskä = float(input("Anna leiviskät: "))
 naulat = float(input("Anna naulat: "))
 luodit = float(input("Anna luodit: "))
 
-#Muunnetaan leiviskät nauloiksi
-naulat_total = leiviskä * 20 + naulat
+#Muunnetaan leiviskät nauloiksi ja naulat luodeista grammoiksi
+naulat_total = (leiviskä * 20) + naulat
 luodit_total = naulat_total * 32
 grammat_total = luodit_total * 13.3
-kg_total = grammat_total/1000
+kg = grammat_total //1000
+g = grammat_total %1000
 print(grammat_total)
-print(kg_total)
-
-#Matikalla erotellaan kilot ja grammat
-g2, kg = math.modf(kg_total)
-kg_grammoiksi = kg*1000
-g = grammat_total-kg_grammoiksi
 
 #Tulos
-print(f"Tulos: {int(kg)}kg ja {g:.0f} grammaa")
+print(f"Tulos: {kg:.0f}kg {round(g)}g")
